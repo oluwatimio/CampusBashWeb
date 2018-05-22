164f06c1b2e19a0732409abd1f8bd9c16c71d0c6
bamboozooled <owoturotimi@live.co.uk>
2018-05-22 19:41:43 -0400

    first commit

---
+++ src/app/event-view/event-view.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EventComponent } from './event/event.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [EventComponent]
+})
+export class EventViewModule { }
---
+++ src/app/interests/interests.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InterestsComponent } from './interests/interests.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [InterestsComponent]
+})
+export class InterestsModule { }
