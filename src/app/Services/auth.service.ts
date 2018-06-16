7dc5db71ff097a6281ffd1a010d408cde8e31e0b
owoturotimi@live.co.uk <owoturotimi@live.co.uk>
2018-06-16 00:28:30 -0400

    added firebase auth

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Injectable()
 export class AuthService {
 
   constructor() { }
+
+  signUp(email: string, pass: string) {
+    firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
+      console.log(error.message);
+    });
+  }
+
+  signIn(email: string, pass: string) {
+    firebase.auth().signInWithEmailAndPassword(email, pass).catch(function(error) {
+      console.log(error.message);
+    });
+  }
 
 }
